1b42266262f36eee7c964c2d8baba785bfce4357
Austin Hamilton <HAMILTONAJ@ETSU.EDU>
2022-09-22 16:56:43 -0400

    Completed the Person and Address classes

--- CSCI 2910 Lab 4/FileRoot.cs
+++ CSCI 2910 Lab 4/FileRoot.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CSCI_2910_Lab_4
 {
-    internal class FileRoot
+    public class FileRoot
     {
         private string _root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
         public string Root { get { return _root; } }
---
+++ CSCI 2910 Lab 4/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI_2910_Lab_4
+{
+    public class Person : IComparable
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public Address Address { get; init; }
+
+        public Person(string firstName, string lastName, Address address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            //cast the object as a Person and then compare their last names
+            Person other = obj as Person;
+            return this.LastName.CompareTo(other.LastName);
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName}|{LastName}|{Address.ToString()}";
+        }
+    }
+}
