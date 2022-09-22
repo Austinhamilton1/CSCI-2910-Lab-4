6df6d0bad2307b9e4b339ad36364ce9db87a6d1f
Austin Hamilton <HAMILTONAJ@ETSU.EDU>
2022-09-22 16:44:18 -0400

    FileRoot -> returns the root directory of the project

---
+++ CSCI 2910 Lab 4/FileRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI_2910_Lab_4
+{
+    internal class FileRoot
+    {
+        private string _root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+        public string Root { get { return _root; } }
+    }
+}
